82361ad8086a37e16dfebf14146d288f4246402a
akimitsu nakajima <a-nakajima@shokuryu.co.jp>
2015-04-09 14:23:32 +0900

    ファイル情報取得関数

--- atd.d/atd.path.h
+++ atd.d/atd.path.h
@@ -31,6 +31,34 @@ struct path : public object
 
 	//ディレクトリの作成
 	static bool mkdir(const string &path, int mode = 0777);
+
+	//ファイル情報
+	struct fileinfo_t 
+	{
+		int dev		;/*st_dev;		ファイルがあるデバイスの ID */
+		int ino		;/*st_ino;		inode 番号 */
+		int mode	;/*st_mode;		アクセス保護 */
+		int nlink	;/*st_nlink;	ハードリンクの数 */
+		int uid		;/*st_uid;		所有者のユーザー ID */
+		int gid		;/*st_gid;		所有者のグループ ID */
+		int rdev	;/*st_rdev;		デバイス ID (特殊ファイルの場合) */
+		int size	;/*st_size;		全体のサイズ (バイト単位) */
+		int blksize	;/*st_blksize;	ファイルシステム I/O でのブロックサイズ */
+		int blocks	;/*st_blocks;	割り当てられた 512B のブロック数 */
+
+		/* Linux 2.6 以降では、カーネルは以下のタイムスタンプ
+		   フィールドでナノ秒の精度をサポートしている。
+		   Linux 2.6 より前のバージョンでの詳細は
+		   下記の「注意」を参照。 */
+
+		datetime atime;/*st_atim  最終アクセス時刻 */
+		datetime mtime;/*st_mtim  最終修正時刻 */
+		datetime ctime;/*st_ctim  最終状態変更時刻 */
+
+		fileinfo_t();
+		void demo();
+	};
+	static fileinfo_t fileinfo(const string &path);
 };
 }//namespace atd
 #endif//__atd_path_h__
