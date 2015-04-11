2e8b839ac61546d8fa394a03302ee2b3b3e21926
akimitsu nakajima <a-nakajima@shokuryu.co.jp>
2015-04-11 17:20:41 +0900

    FDGの配列化ロジック

--- atd.d/test.cpp
+++ atd.d/test.cpp
@@ -3,6 +3,8 @@
 #include "cobol.h"
 #include "thread.h"
 
+
+//####################################################
 void test(
 	string ebc
 	, const strings &fdgs
@@ -11,5 +13,30 @@ void test(
 	, bool looksuffix
 )
 {
+	path::fileinfo_t info = path::fileinfo(ebc);
+	notify("");
 	notify(ebc);
+	info.demo(notify);
+
+	int rsize = 0;
+	struct fdglist : public std::vector<cobol::fdg>
+	{
+		cobol::fdg &add(const string &path)
+		{
+			push_back(cobol::fdg());
+			cobol::fdg &fdg = back();
+			std::ifstream ifs(path.c_str(), std::ios::in);
+			fdg.loadcobol(ifs);
+			return fdg;
+		}
+	} fdglist;
+	for (strings::const_iterator i = fdgs.begin(), e = fdgs.end()
+		; i != e; ++i)
+	{
+		const string &path = *i;
+		cobol::fdg &fdg = fdglist.add(path);
+		notify("");
+		fdg.demo(notify);
+	}
 }
+//####################################################
