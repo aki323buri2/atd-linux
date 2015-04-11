58f9e935238b6ae78438bb677e1d187d918914b3
akimitsu nakajima <a-nakajima@shokuryu.co.jp>
2015-04-11 13:41:24 +0900

    安直なグローバル関数をやめてクラスの静的メンバにする

--- atd.d/common.h
+++ atd.d/common.h
@@ -4,7 +4,13 @@
 #include "atd.h"
 using namespace atd;
 
-void test(const string &text);//★
+void test(
+	string ebc
+	, const strings &fdgs
+	, const strings &keys
+	, const strings &jsons
+	, bool looksuffix
+);//★
 
 static struct app
 {
@@ -58,64 +64,66 @@ struct commandline : public generic::properties
 //====================================================
 //=　search fdg path
 //====================================================
-string searchfdg(const string &path)
-{	
-	string name = path::basename(path);
-	string root = path::dirname(path);
-	//検索対象ディレクトリリスト
-	strings dirs(root);
-	dirs.entry(path::dirname(root) + "/fdg");//../fdg
+struct searchfdg
+{
+	static string search(const string &path)
+	{	
+		string name = path::basename(path);
+		string root = path::dirname(path);
+		//検索対象ディレクトリリスト
+		strings dirs(root);
+		dirs.entry(path::dirname(root) + "/fdg");//../fdg
 
-	//ファイル名の検索対象リスト
-	strings names(name);
+		//ファイル名の検索対象リスト
+		strings names(name);
 
-	//名前と修飾子（ライブラリ名）に分割
-	struct { string name, lib; } split;
-	strings match;
-	if (regex::match("^(.+)\\.([^\\.]+)$", name, match))
-	{
-		split.name = match[1];
-		split.lib  = match[2];
-		names.entry(split.name);
-	}
-	else 
-	{
-		split.name = name;
-		split.lib  = "";
-	}
-	//RDMLIBは特別！★
-	const string abslib = "RDMLIB";
-	if (split.lib != abslib) names.entry(split.name + "." + abslib);
+		//名前と修飾子（ライブラリ名）に分割
+		struct { string name, lib; } split;
+		strings match;
+		if (regex::match("^(.+)\\.([^\\.]+)$", name, match))
+		{
+			split.name = match[1];
+			split.lib  = match[2];
+			names.entry(split.name);
+		}
+		else 
+		{
+			split.name = name;
+			split.lib  = "";
+		}
+		//RDMLIBは特別！★
+		const string abslib = "RDMLIB";
+		if (split.lib != abslib) names.entry(split.name + "." + abslib);
 
 
-	//ディレクトリリスト x ファイル名リストで検索
-	struct { bool check; string path; } hit = { false, "" };
-	for (strings::const_iterator i = dirs.begin(), e = dirs.end()
-		; i != e; ++i)
-	{
-		//ディレクトリ名
-		const string &dir = *i;
-
-		for (strings::const_iterator i = names.begin(), e = names.end()
+		//ディレクトリリスト x ファイル名リストで検索
+		struct { bool check; string path; } hit = { false, "" };
+		for (strings::const_iterator i = dirs.begin(), e = dirs.end()
 			; i != e; ++i)
 		{
-			//ファイル名
-			const string &name = *i;
+			//ディレクトリ名
+			const string &dir = *i;
 
-			//存在チェック
-			string path = dir + "/" + name + ".FDG.txt"; //★
-			if (path::exists(path))
+			for (strings::const_iterator i = names.begin(), e = names.end()
+				; i != e; ++i)
 			{
-				hit.check = true;
-				hit.path = path;
-				break;
-			}
-		}
-		if (hit.check) break;
-	}
-	return hit.path;
-}
+				//ファイル名
+				const string &name = *i;
 
+				//存在チェック
+				string path = dir + "/" + name + ".FDG.txt"; //★
+				if (path::exists(path))
+				{
+					hit.check = true;
+					hit.path = path;
+					break;
+				}
+			}
+			if (hit.check) break;
+		}
+		return hit.path;
+	}
+};
 //====================================================
 //= fcheck
 //====================================================
